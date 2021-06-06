95efce6b23c7bf290924fd039c9f0e86fbb7e2ff
Justin Lս <lljustinlu@gmail.com>
2021-06-06 16:05:44 +1000

    ?

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import { Pressable, TextInput, View } from 'react-native';
+import { Pressable, TextInput, View, StyleSheet } from 'react-native';
 import {Formik, useField} from 'formik';
 import Text from './Text';
 import * as yup from 'yup';
+
+const styles = StyleSheet.create({
+  signInButton: {
+    backgroundColor: '#0366d6',
+    width: '20%',
+    alignSelf: 'center',
+    borderRadius: 5
+  },
+
+  formOutline: {
+    borderRadius: 5,
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    width: '95%',
+    alignSelf: 'center'
+    
+  },
+  emptySpacing:{
+    padding: 5
+  }
+})
 
 const validationSchema = yup.object().shape({
   username: yup.string().required('username is required'),
@@ -15,7 +37,7 @@ const initialValues = {
 }
 
 const onSubmit = (values) => {
-  console.log(values);
+  console.log('zubmitted ' + values);
 }
 
 const SignInForm = () => {
@@ -24,18 +46,26 @@ const SignInForm = () => {
 
   return(
     <View>
+      <View style = {styles.emptySpacing}/>
+      <View style = {styles.formOutline}>
       <TextInput
         placeholder = "Username"
         value = {userField.value}
         onChangeText = {text => userHelpers.setValue(text)}
       />
+      </View>
+      <View style = {styles.emptySpacing}/>
+      <View style = {styles.formOutline}>
       <TextInput
+        secureTextEntry = {true}
         placeholder = "Password"
         value = {passField.value}
         onChangeText = {text => passHelpers.setValue(text)}
       />
-      <Pressable onPress = {onSubmit}>
-        <Text>Submit</Text>
+      </View>
+      <View style = {styles.emptySpacing}/>
+      <Pressable onPress = {onSubmit(userField.value + passField.value)} style = {styles.signInButton}>
+        <Text fontSize = 'subheading' fontWeight = 'bold' color = 'textSecondary' style = {{alignSelf: 'center'}}>Sign in</Text>
       </Pressable>
     </View>
   )
